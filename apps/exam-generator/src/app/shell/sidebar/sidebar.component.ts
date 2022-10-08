3db5ce568d4f9d7791fd48037f7b498694ace234
Andreas Schmid <andreas.schmid13@googlemail.com>
2022-10-08 18:10:45 +0200

    optimize shell

--- apps/exam-generator/src/app/shell/sidebar/sidebar.component.ts
+++ apps/exam-generator/src/app/shell/sidebar/sidebar.component.ts
@@ -5,7 +5,7 @@ import { MenuItem } from "./menu-item.model";
 @Component({
   selector: "app-sidebar",
   templateUrl: "./sidebar.component.html",
-  styleUrls: ["./sidebar.component.scss"],
+  styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent implements OnInit {
   public menuItems: MenuItem[] = [];
