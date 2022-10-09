84ae1dd34b3f32ceba08e24877fec91a4ac079d7
Andreas Schmid <andreas.schmid13@googlemail.com>
2022-10-09 19:31:29 +0200

    working delete button

---
+++ apps/exam-generator/src/app/task/exam-task-not-found/exam-task-not-found.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { v4 as uuidv4 } from "uuid";
+
+@Component({
+  selector: "app-exam-task-not-found",
+  templateUrl: "./exam-task-not-found.component.html"
+})
+export class ExamTaskNotFoundComponent {
+  constructor(private router: Router) {}
+
+  public async createNewTask() {
+    const newId = uuidv4();
+    await this.router.navigate([`tasks/${newId}/edit`], {
+      queryParams: {
+        new: true
+      }
+    });
+  }
+}
---
+++ apps/exam-generator/src/app/task/exam-task-not-found/exam-task-not-found.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ExamTaskNotFoundComponent } from "./exam-task-not-found.component";
+
+@NgModule({
+  declarations: [ExamTaskNotFoundComponent],
+  imports: [CommonModule],
+})
+export class ExamTaskNotFoundModule {}
